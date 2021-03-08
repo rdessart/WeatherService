94d295634bcd9b8d00ec1bd36216668e47a85d84
Romain DESSART <romaindessart@gmail.com>
2021-03-08 09:37:10 +0100

    Add project files.

---
+++ GlobalForcastSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GlobalForcastSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
