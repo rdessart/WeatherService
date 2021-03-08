59f856ef83f8f5240e50ba5cb236a4e9ddb2bf5f
Romain DESSART <romaindessart@gmail.com>
2021-03-08 15:08:33 +0100

    Testing download url generation

--- GlobalForcastSystem/Program.cs
+++ GlobalForcastSystem/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
-
+using System.Threading.Tasks;
 namespace GlobalForcastSystem
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            Console.WriteLine(GFS.GetLatestGFSUrl());
+            //Console.WriteLine("GFS PARSER 1.0.0.0");
+            //GFS gfs = new GFS();
+            //bool res = await gfs.GetData();
+            //gfs.DisplayGrids();
+            //Console.WriteLine(res);
         }
+
+        
     }
 }
